db021ad18834171c8dc44b26e6775f3daa8706c7
bereket <beki4work@gmail.com>
2024-04-12 09:23:19 +0300

    Navigation

---
+++ mobile/bereket_asnake/ecommerce/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce/add_product.dart';
+import 'package:ecommerce/homePage.dart';
+import 'package:ecommerce/detail.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ecommerce app',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/add_product': (context) => AddProduct(),
+        '/product_detail': (context) => const Detail(),
+        '/edit_product': (context) => AddProduct(edit: true)
+      },
+    );
+  }
+}
